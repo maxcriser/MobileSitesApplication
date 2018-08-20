b18c69021fad228e0f54139a45b7a8759fbb5664
Maksim Zakharenka <Maksim_Zakharenka@epam.com>
2018-08-20 18:32:35 +0300

    broower landing application android

---
+++ BroowerLanding/app/src/main/java/landing/broower/by/broower/Constants.java
@@ -0,0 +1,14 @@
+package landing.broower.by.broower;
+
+public class Constants {
+
+    interface URL {
+
+        String URL_HOST = "https://broower.github.io";
+    }
+
+    interface ANIMATION {
+
+        int FADE_OUT_MILLIS = 750;
+    }
+}
---
+++ BroowerLanding/app/src/main/java/landing/broower/by/broower/SplashActivity.java
@@ -0,0 +1,17 @@
+package landing.broower.by.broower;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
